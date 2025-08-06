b02b8875c751f44ef95ef62cfec3ef439b03ddae
NightDev701 <lukasbaehr01@gmail.com>
2025-08-06 18:03:29 +0200

    add "/experimental"

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 
 require('dotenv').config();
 
+const rootDir = path.join(__dirname, 'files');
+const experimantalDir = path.join(__dirname, 'experiment');
+
 const isDevMode = process.env.NODE_ENV === 'development';
 const PORT = isDevMode ? 8080 : 80;
 
@@ -25,8 +28,35 @@ app.get('/downloads', (req, res) => {
 app.get('/minecraft', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'minecraft_server.html'));
 });
+app.get('/experimental/*', (req, res) => {
+    const requestPath = req.params[0];
+    const filePath = path.join(experimantalDir, decodeURIComponent(requestPath));
 
-const rootDir = path.join(__dirname, 'files');
+    console.log(`Request detected: ${req.method} ${req.url}`);
+
+    fs.stat(filePath, (err, stats) => {
+        if (err || !stats.isFile()) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('File not found');
+            return;
+        }
+
+        let contentType = mime.lookup(filePath) || 'application/octet-stream';
+
+        if (path.extname(filePath) === '.slsd') {
+            contentType = 'text/plain';
+        }
+
+        console.log(`File found and sent back to target ${filePath} with content-type ${contentType}`);
+
+        res.writeHead(200, {
+            'Content-Type': contentType,
+            'Content-Length': stats.size
+        });
+
+        fs.createReadStream(filePath).pipe(res);
+    });
+});
 
 app.get('/api/sunlightscorpion/*', (req, res) => {
     const requestPath = req.params[0];
