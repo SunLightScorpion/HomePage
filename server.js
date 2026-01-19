c96e224054a53fbe479797d107df44aafa7a2164
NightDev701 <lukasbaehr01@gmail.com>
2026-01-19 22:45:14 +0100

    small changes

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});/*
-app.get('/game', (req, res) => {
+});
+/*app.get('/game', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'game.html'));
 });*/
 app.get('/downloads', (req, res) => {
